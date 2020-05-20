41382dc071929b29b63907d7da5ef437015da107
anoronha <noronha.aarati@gmail.com>
2020-05-20 14:13:25 -0400

    Update 21.cpp

--- 21.cpp
+++ 21.cpp
@@ -29,27 +29,39 @@ class Solution {
 public:
    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
           
-    ListNode* result = NULL;  
-      
-    
-    if (l1 == nullptr)  
-        return(l2);  
-    else if (l2 == nullptr)  
-        return(l1);  
-      
-    
-    if (l1->val <= l2->val)  
-    {  
-        result = l1; 
-        
-        result->next = mergeTwoLists(l1->next, l2);  
-    }  
-    else
-    {  
-        result = l2;  
-        result->next = mergeTwoLists(l1, l2->next);  
-    }  
-    return(result);  
+   ListNode start(0);
+   ListNode * newl = &start;
+   while ((l1 != nullptr) || (l2 != nullptr)){
+        if (l1 == nullptr){
+            int val = l2->val;
+            newl->next = new ListNode(val);
+            newl = newl->next;
+            l2 = l2->next;
+       }
+       
+       else if (l2 == nullptr){
+            int val = l1->val;
+            newl->next = new ListNode(val);
+            newl = newl->next;
+            l1 = l1->next;
+       }
+       else if (l1->val < l2->val){
+                int val = l1->val;
+                newl->next = new ListNode(val);
+                newl = newl->next;
+                l1 = l1->next;
+       }
+       
+       else {
+                int val = l2->val;
+                newl->next = new ListNode(val);
+                newl = newl->next;
+                l2 = l2->next;
+       }
+       
+   }
+    return start.next;
+   
 }
 };
     
